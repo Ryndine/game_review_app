d3cf0a0c616c9624cfd0b0b98916cba2be8c8167
Ryndine <rcmangeno@gmail.com>
2023-01-17 14:39:19 -0800

    Route for adding new campgrounds. Basic navigation.

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const Campground = require('./models/campground')
+const Campground = require('./models/campground');
+const campground = require('./models/campground');
 
 mongoose.set('strictQuery', false); // deprecation warning 
 mongoose.connect('mongodb://localhost:27017/campground-app', {
@@ -22,18 +23,39 @@ db.once('open', () => {
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views')
-)
+app.set('views', path.join(__dirname, 'views'));
+
+// parse the "body"
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.render('home')
+});
+
+// list of all the campgrounds
+app.get('/campgrounds', async(req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
+});
+
+// create new campground
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
 })
 
-app.get('/campgrounds', async (req, res) => {
-    const campgrounds = await Campground.find({})
-    res.render('campgrounds/index', { campgrounds })
+app.post('/campgrounds', async(req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
 })
+
+// view a single campground
+app.get('/campgrounds/:id', async(req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', { campground });
+});
+
 
 app.listen(3000, () => {
-    console.log('Serving on port 3000')
-})
+    console.log('Serving on port 3000');
+});
