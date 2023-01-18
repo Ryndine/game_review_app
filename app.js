01774dd0f50f38205cbeae8338755c84236e2386
Ryndine <rcmangeno@gmail.com>
2023-01-17 16:36:26 -0800

    Adding update functionality

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
-const campground = require('./models/campground');
 
 mongoose.set('strictQuery', false); // deprecation warning 
 mongoose.connect('mongodb://localhost:27017/campground-app', {
@@ -25,8 +25,10 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(methodOverride('_method'))
 // parse the "body"
 app.use(express.urlencoded({extended: true}));
+
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -55,6 +57,17 @@ app.get('/campgrounds/:id', async(req, res) => {
     res.render('campgrounds/show', { campground });
 });
 
+// edit campground
+app.get('/campgrounds/:id/edit', async(req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', { campground });
+})
+
+app.put('/campgrounds/:id', async(req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${campground._id}`)
+});
 
 app.listen(3000, () => {
     console.log('Serving on port 3000');
